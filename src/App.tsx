0ae46646a6cbd56b93d9620f44924ea3ad918a7c
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2025-03-29 15:35:58 -0300

    style(global) removed initial global styles and configured mui theme light/dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,39 @@
-import { Box, Container } from '@mui/material';
+import {
+  Box,
+  Container,
+  createTheme,
+  CssBaseline,
+  GlobalStyles,
+  ThemeProvider,
+} from '@mui/material';
 
 import Header from './components/Header';
 import AppRoutes from './routes/AppRoutes';
+import { useAppTheme } from './stores/useAppTheme';
+import getTheme from './themes';
 
 function App() {
+  const { currentTheme } = useAppTheme();
+
+  const theme = getTheme(currentTheme);
+
   return (
-    <div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <GlobalStyles
+        styles={(theme) => ({
+          body: {
+            backgroundColor: theme.palette.background.default,
+          },
+        })}
+      />
       <Header />
       <Container maxWidth="lg">
         <Box mt={4}>
           <AppRoutes />
         </Box>
       </Container>
-    </div>
+    </ThemeProvider>
   );
 }
 
