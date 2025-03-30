d1cc36787d33ac542fbb86560703664817310c7a
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2025-03-30 11:26:15 -0300

    refactor(error-message):  add message on error creating company

--- src/schemas/companySchema.ts
+++ src/schemas/companySchema.ts
@@ -1,20 +1,35 @@
 import { z } from 'zod';
 
 export const companySchema = z.object({
-  name: z.string().min(1, 'Nome é obrigatório'),
-  cnpj: z.string().min(18, 'CNPJ inválido'),
-  tradeName: z.string().min(1, 'Nome Fantasia é obrigatório'),
+  name: z
+    .string({ required_error: 'Nome é obrigatório' })
+    .min(2, 'Nome precisa conter 2 caracteres'),
+  cnpj: z
+    .string({ required_error: 'CNPJ é obrigatório' })
+    .min(18, 'CNPJ inválido'),
+  tradeName: z
+    .string({ required_error: 'Nome Fantasia é obrigatório' })
+    .min(1, 'Nome precisa conter 2 caracteres'),
 
-  logradouro: z.string().min(1, 'Logradouro é obrigatório'),
-  numero: z.string().min(1, 'Número é obrigatório'),
+  logradouro: z
+    .string({ required_error: 'Logradouro é obrigatório' })
+    .min(1, 'Logradouro é obrigatório'),
+  numero: z
+    .string({ required_error: 'Número é obrigatório' })
+    .min(1, 'Número é obrigatório'),
   complemento: z.string().optional(),
-  bairro: z.string().min(1, 'Bairro é obrigatória'),
-  cep: z.string().min(9, 'CEP é obrigatório'),
-  municipio: z.string().min(1, 'Município é obrigatório'),
-  estado: z.string().min(2, 'Estado é obrigatório'),
-
-  criadoEm: z.date().optional(),
-  alteradoEm: z.date().optional(),
+  bairro: z
+    .string({ required_error: 'Bairro é obrigatória' })
+    .min(1, 'Bairro é obrigatória'),
+  cep: z.string({ required_error: 'CEP é obrigatório' }).min(9, 'CEP inválido'),
+  municipio: z
+    .string({ required_error: 'Município é obrigatório' })
+    .min(1, 'Município é obrigatório'),
+  estado: z
+    .string({ required_error: 'Estado é obrigatório' })
+    .min(2, 'Estado é obrigatório'),
 });
 
 export type CompanyFormData = z.infer<typeof companySchema>;
+
+// ref solve problem https://stackoverflow.com/questions/76672351/error-messages-from-react-hook-form-with-zod
