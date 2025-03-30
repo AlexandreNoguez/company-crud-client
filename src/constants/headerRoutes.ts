fecbacb7f1363881b5e88de658b077bf571adbbc
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2025-03-29 22:48:56 -0300

    refactor(routes): add constants to route file

---
+++ src/constants/headerRoutes.ts
@@ -0,0 +1,5 @@
+export const ROUTE_EMPRESAS = '/empresas';
+export const ROUTE_EMPRESAS_CADASTRAR = '/empresas/cadastrar';
+export const ROUTE_EMPRESAS_EDITAR = '/empresas/editar/:id';
+export const ROUTE_HOME = '/';
+export const ROUTE_NOT_FOUND = '*';
