8d90af28fc72a9535ab1be9b93b567e20755ab32
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2025-03-30 21:46:58 -0300

    fix(Icon): add button props for style

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -1,14 +1,14 @@
-import { IconButton } from '@mui/material';
+import { ButtonProps, IconButton } from '@mui/material';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
 
 import { useAppTheme } from '../stores/useAppTheme';
 
-export function ThemeToggleButton() {
+export function ThemeToggleButton(props: ButtonProps) {
   const { currentTheme, toggleTheme } = useAppTheme();
 
   return (
-    <IconButton onClick={toggleTheme} color="inherit">
+    <IconButton onClick={toggleTheme} color="inherit" {...props}>
       {currentTheme === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
     </IconButton>
   );
